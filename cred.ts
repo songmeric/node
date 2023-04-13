24841e84f81b8458cc2cc89d0f3c3d89c9500c0c
songmeric <77728557+songmeric@users.noreply.github.com>
2023-04-13 16:39:13 +0900

    Update and rename accounts.txt to cred.ts

---
+++ cred.ts
@@ -0,0 +1,24 @@
+export const DEMUX_L_UUID:string = 'hKDgNHpyHo5wUoIqQljd/VU7roHHJtjADYR9B4+yCUKuBOZCwDaLxxMDruaKwi9VZA7En8xjCCim3LvE1iPYeg==';
+export const DEMUX_L_NAME:string = 'DESKTOP-AMAAMA';
+export const DEMUX_L_EMAIL:string = '01084433988';
+export const DEMUX_L_PASSWORD:string = 'ssj48089749';
+
+export const DEMUX_MOD_CLIENT_UUID:string = 'AdYHaXs/i50lv1qDwK1t9Pa76kXIPgnJbwM4VMHl+aSfxg36XIggMRs7ztMh/zHq5/ka5XBaHScxJ5pTuLgvax==';
+export const DEMUX_MOD_CLIENT_NAME:string = 'DESKTOP-F12A12';
+export const DEMUX_MOD_CLIENT_EMAIL:string = '01056490575';
+export const DEMUX_MOD_CLIENT_PASSWORD:string = 'aawwxx5649';
+
+export const BR_LISTENER_UUID:string = 'KmCzeei6Jb64Hl40xcyUXzOmnRFiEFRZ+ys05ZbEnL3900kvDJK8WW429urRipRneRHRhzt7k4vMLtIwX+pCUd==';
+export const BR_LISTENER_NAME:string = 'DESKTOP-61AZIH';
+export const BR_LISTENER_EMAIL:string = '01083566910';
+export const BR_LISTENER_PASSWORD:string = 'people123!@';
+
+export const BR_CLIENT_1_UUID:string = 'jQtpoOMzhpUeNBZe+3CclDAxqxXwyRE0gdQTdQ1X0/lqQxKdNUPc0O1B3+0G3Bf9dlDQUJ3Tf1+L2HXI1OeySP==';
+export const BR_CLIENT_1_NAME:string = 'DESKTOP-71AZIZ';
+export const BR_CLIENT_1_EMAIL:string = '01059097270';
+export const BR_CLIENT_1_PASSWORD:string = 'zzxxcc7270';
+
+export const BR_CLIENT_2_UUID:string = 'EYVplIgOtHOueyOO0TqJV4hDdEuY24VACVdYX5Zk8V1Tmcnp0EnfIP0HBvRqIjQ0KpUPjpYfwhivwUS0n/48hn==';
+export const BR_CLIENT_2_NAME:string = 'DESKTOP-81BZIH';
+export const BR_CLIENT_2_EMAIL:string = '01082731830';
+export const BR_CLIENT_2_PASSWORD:string = 'zzxxcc1830';
